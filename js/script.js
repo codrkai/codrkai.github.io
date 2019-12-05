e99f48a5f20e55f74073c1a1ebfd22f5bb717994
Johnny <codrkai@gmail.com>
2019-12-04 21:36:57 -0800

    bootstrap 4 website

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    $('#header-include').load('https://codrkai.github.io/header.html');
+    $('#footer-include').load('https://codrkai.github.io/footer.html');
+
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 50 ) {
+            $('.navbar').addClass('solid bg-dark');
+        } else {
+            $('.navbar').removeClass('solid bg-dark');
+        }
+    });
+
+    $('.dropdown-item, .nav-link').on('click', function(e) {
+        if ( this.hash !== "" ) {
+            e.preventDefault();
+            const anchor = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(anchor).offset().top
+            }, 800, function() {
+                window.location.hash = anchor;
+            });
+        }
+    });
+});
