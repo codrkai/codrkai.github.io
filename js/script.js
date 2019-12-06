4cd336fcbf9a73441984ffe4fce4d179756ade07
Johnny <codrkai@gmail.com>
2019-12-05 16:53:36 -0800

    bootstrap 4 website

--- js/script.js
+++ js/script.js
@@ -9,4 +9,24 @@ $(function() {
             $('.navbar').removeClass('solid bg-dark');
         }
     });
+
+    // This script has been moved to footer.html because since I'm
+    // using the javascript load() function above, this now needs to 
+    // run after everthing is loaded.
+    // If you are not using the load() function above, then uncomment 
+    // the function below and it will work the same way.
+    /*
+    $('.dropdown-item, .nav-item').on('click', function(e) {
+        if ( this.hash !== "" ) {
+            e.preventDefault();
+            var anchor = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(anchor).offset().top
+            }, 800, function() {
+                window.location.hash = anchor;
+            });
+        }
+    });
+    */
 });
